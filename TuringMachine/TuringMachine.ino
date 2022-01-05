b3cac7262043f846e9aca64050192dfa292f8d58
Botxan <oihan.ic@gmail.com>
2022-01-05 01:36:20 +0100

    Added SD Reader and RGB Status LED

--- TuringMachine/TuringMachine.ino
+++ TuringMachine/TuringMachine.ino
@@ -1,9 +1,11 @@
-#define MAX_TRANSITIONS 50
-#define MAX_ALPHABET_SYMBOLS 27
-#define TAPE_LENGTH 128
+#define MAX_TRANSITIONS 60 // limited dude lack of dynamic memory
+#define MAX_ALPHABET_SYMBOLS 30 // limited dude lack of dynamic memory
+#define TAPE_LENGTH 128 // limited dude lack of dynamic memory
+#define MAX_MACHINES 10 // limited dude lack of dynamic memory
 
 // States
-#define SETUP 0
+#define START -1
+#define MACHINE_SELECTION 0
 #define INSERT_INPUT 1
 #define RUN 2
 #define HALTED 3
@@ -12,25 +14,40 @@
 #define BLANK '*'
 
 /****************** LIBRARIES ******************/
-// library for liquid cristal display
+// sd card
+#include <SPI.h>
+#include <SD.h>
+
+// liquid cristal display
 #include <LiquidCrystal.h>
 // initialize liquid crystal display library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 /****************** CONSTANTS ******************/
 // buttons
-const int b_back = 6;
-const int b_left = 7;
-const int b_right = 8;
-const int b_enter = 9;
-const int b_next_state = 10;
+const int8_t b_back = 22;
+const int8_t b_left = 23;
+const int8_t b_right = 24;
+const int8_t b_enter = 25;
+const int8_t b_next_state = 26;
+
+//rgb led
+int red_light_pin = 9;
+int green_light_pin = 10;
+int blue_light_pin = 11;
 
 /****************** STRUCTS ******************/
+// names of read machines
+struct machines {
+    String names[MAX_MACHINES];
+    int size;
+};
+
 // accepted alphabet
 struct alphabet {
     char symbols[MAX_ALPHABET_SYMBOLS];
-    int size;
+    int8_t size;
 };
 
 // represents a transition
@@ -45,7 +62,7 @@ struct transition {
 // used to store the transition table
 struct transitions {
     transition transitions[MAX_TRANSITIONS];
-    int size;
+    short size;
 };
 
 /****************** CLASSES ******************/
@@ -60,49 +77,108 @@ class Machine {
 };
 
 /****************** VARIABLES ******************/
-// Turing machine instance
+// turing machine instance
 Machine machine;
 
 // phase where the program is
-int phase;
+int8_t phase = START;
+
+// phase of selecting machines
+machines machines;
+int8_t selectedMachine = 0;
+File file;
 
 // input phase variables
-int inputCursor; // cursor position in the sreen
+int8_t inputCursor = 0; // cursor position in the sreen
 boolean selectSymbolMode = false; // true <=> selecting symbol. Otherwise, traversing the tape
-int selectedCharIndex = 0; // cycle between alphabet
+int8_t selectedCharIndex = 0; // cycle between alphabet
 
 long drawMillis = 0; // Millis tracker for drawing
 long buttonMillis = 0; // Millis tracker for buttons
-long interval = 500; // Interval at which update (milliseconds)
+short interval = 500; // Interval at which update (milliseconds)
 
 /**
  * Initialize the buttons, the lcd, reset the tape and build the machine
  */
 void setup() {
+    // Open serial communications and wait for port to open:
     Serial.begin(9600);
+    RGB_color(255, 165, 0);
     
-    // initialise buttons as inputs
+    // initialise buttons
     pinMode(b_back, INPUT);
     pinMode(b_left, INPUT);
     pinMode(b_right, INPUT);
     pinMode(b_enter, INPUT);
     pinMode(b_next_state, INPUT);
 
-    phase = 1;
-    inputCursor = 0;
-    
+    // intialise RGB led
+    pinMode(red_light_pin, OUTPUT);
+    pinMode(green_light_pin, OUTPUT);
+    pinMode(blue_light_pin, OUTPUT); 
+
     // select number of rows and collumns that the lcd will use
     lcd.begin(16, 2);
-    // enable display of the cursor in the lcd screen
-    lcd.cursor();
+    
+    // If sd card is not found
+    if (!SD.begin(53)) {
+        lcd.setCursor(0, 0);
+        lcd.write("SD card");
+        lcd.setCursor(0, 1);
+        lcd.write("not found");
+        RGB_color(255, 0, 0);
+        while(1);
+    }
+
+    // After detecting the SD cards select the machine
+    lcd.setCursor(0, 0);
+    lcd.write("Reading machines");
+    lcd.setCursor(0,1);
+    lcd.write("from SD card...");
+    delay(1000);
+
+    importMachineNames();
+
+    lcd.clear();
+    
+    // stop if there are no machines to use
+    if (machines.size == 0) {
+        lcd.setCursor(0, 0);
+        lcd.write("No machine found");
+        RGB_color(255, 0, 0);
+        while(1);
+    }
+
+    // enter MACHINE_SELECTION phase
+    phase = MACHINE_SELECTION;
+    RGB_color(0, 255, 0);
+    selectedMachine = 0;
 
     // reset the tape
     resetTape();
-    
-    // add machine alphabet and transition table
-    buildMachine();
 }
 
+/**
+ * Reads file names from the SD Card
+ */
+void importMachineNames() {
+    RGB_color(255, 165, 0);
+    // open root directory
+    File root = SD.open("/");
+
+    int i = 0;
+    while (i < MAX_MACHINES) {
+        File entry = root.openNextFile();
+        if (!entry) break; // no more entries
+        machines.names[i] = entry.name();
+        i++;
+    }
+    machines.size = i;
+}
+
+/**
+ * Restart the tape with empty '*' symbols
+ */
 void resetTape() {
     // mark all cells as blank
     for (int i = 0; i < TAPE_LENGTH; i++) machine.tape[i] = BLANK;
@@ -111,63 +187,98 @@ void resetTape() {
 /**
    Add the alphabet and transition function of the alphabet
 */
-void buildMachine() { 
+void buildMachine() {
+    RGB_color(255, 165, 0);
+    lcd.setCursor(0, 0);
+    lcd.print("Building the");
+    lcd.setCursor(0,1);
+    lcd.print("machine. Wait...");
+
+    char c;
+    String token;
+    
     // set machine current state
     machine.currentState = "0";
-    
+
+    // Check if file name is correct
+    file = SD.open(machines.names[selectedMachine]);
+
+    if (file) {
+        // read number of symbol in the alphabet
+        token = "";
+        while ((c = file.read()) != '\r') {
+            token += c;
+        }
+        machine.alphabet.size = token.toInt();
+        
+        // skip end of line \r\n
+        file.read();
+        
+        // read accepted alphabet
+        int i = 0;
+        while ((c = file.read()) != '\r') {    
+            if (c != ' ') {
+                machine.alphabet.symbols[i] = c;
+                i++;
+            }
+        }
+
+        file.read(); // skip end of line
+
+        // get the number of transitions
+        token = "";
+        while ((c = file.read()) != '\r') token += c;
+        machine.transition_table.size = token.toInt();
+
+        // skip carriage return and end of line
+        file.read();
+        
+        // read transition table
+        for (int i = 0; i < machine.transition_table.size; i++) {
+            // current state
+            token = "";
+            while ((c = file.read()) != ' ') token += c;
+            machine.transition_table.transitions[i].currentState = token;
+
+            // read symbol
+            machine.transition_table.transitions[i].readSymbol = file.read();
+            file.read();
+            
+            // next state
+            token = "";
+            while ((c = file.read()) != ' ') token += c;
+            machine.transition_table.transitions[i].nextState = token;
+
+            // symbol to write
+            machine.transition_table.transitions[i].writeSymbol = file.read();
+            
+            file.read();
+
+            // direction to move
+            machine.transition_table.transitions[i].dir = file.read(); 
+
+            // skip carriage return and end of line
+            file.read();
+            file.read();
+        }          
+    } else {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("File not found");
+        while(1);
+    }
+    Serial.println(machine.transition_table.size);
     // set initial position
     machine.headPos = TAPE_LENGTH / 2;
+
+    delay(2000);
+    phase = INSERT_INPUT;
+    lcd.clear();
+    lcd.cursor();
     
-    // get accepted alphabet
-    machine.alphabet.size = 3;
-    machine.alphabet.symbols[0] = 'a';
-    machine.alphabet.symbols[1] = 'b';
-    machine.alphabet.symbols[2] = '#';    
-    
-    // get transition table  
-    machine.transition_table.size = 29;
-    
-    machine.transition_table.transitions[0] = {.currentState = "0", .readSymbol = 'a', .nextState = "0", .writeSymbol = 'a', .dir = 'r'};
-    Serial.println(machine.transition_table.transitions[0].currentState);
-    Serial.println(machine.transition_table.transitions[0].currentState);
-    Serial.println(machine.transition_table.transitions[0].currentState);
-    machine.transition_table.transitions[1] = {.currentState = "0", .readSymbol = 'b', .nextState = "0", .writeSymbol = 'b', .dir = 'r'};   
-    machine.transition_table.transitions[2] = {.currentState = "0", .readSymbol = BLANK, .nextState = "1", .writeSymbol = '#', .dir = 'l'};
-    Serial.println(machine.transition_table.transitions[0].readSymbol);
-    
-    machine.transition_table.transitions[3] = {.currentState = "1", .readSymbol = 'a', .nextState = "2", .writeSymbol = '#', .dir = 'r'};
-    machine.transition_table.transitions[4] = {.currentState = "1", .readSymbol = 'b', .nextState = "5", .writeSymbol = '#', .dir = 'r'};
-    machine.transition_table.transitions[5] = {.currentState = "1", .readSymbol = BLANK, .nextState = "8", .writeSymbol = BLANK, .dir = 'r'};
-    
-    machine.transition_table.transitions[6] = {.currentState = "2", .readSymbol = 'a', .nextState = "2", .writeSymbol = 'a', .dir = 'r'};
-    machine.transition_table.transitions[7] = {.currentState = "2", .readSymbol = 'b', .nextState = "2", .writeSymbol = 'b', .dir = 'r'};
-    machine.transition_table.transitions[8] = {.currentState = "2", .readSymbol = '#', .nextState = "2", .writeSymbol = '#', .dir = 'r'};
-    machine.transition_table.transitions[9] = {.currentState = "2", .readSymbol = BLANK, .nextState = "3", .writeSymbol = 'a', .dir = 'l'};
-    
-    machine.transition_table.transitions[10] = {.currentState = "3", .readSymbol = 'a', .nextState = "3", .writeSymbol = 'a', .dir = 'l'};
-    machine.transition_table.transitions[11] = {.currentState = "3", .readSymbol = 'b', .nextState = "3", .writeSymbol = 'b', .dir = 'l'};
-    machine.transition_table.transitions[12] = {.currentState = "3", .readSymbol = '#', .nextState = "4", .writeSymbol = '#', .dir = 'l'};
-    
-    machine.transition_table.transitions[13] = {.currentState = "4", .readSymbol = 'a', .nextState = "4", .writeSymbol = 'a', .dir = 'l'};
-    machine.transition_table.transitions[14] = {.currentState = "4", .readSymbol = 'b', .nextState = "4", .writeSymbol = 'b', .dir = 'l'};
-    machine.transition_table.transitions[15] = {.currentState = "4", .readSymbol = '#', .nextState = "1", .writeSymbol = 'a', .dir = 'l'};
-    
-    machine.transition_table.transitions[16] = {.currentState = "5", .readSymbol = 'a', .nextState = "5", .writeSymbol = 'a', .dir = 'r'};
-    machine.transition_table.transitions[17] = {.currentState = "5", .readSymbol = 'b', .nextState = "5", .writeSymbol = 'b', .dir = 'r'};
-    machine.transition_table.transitions[18] = {.currentState = "5", .readSymbol = '#', .nextState = "5", .writeSymbol = '#', .dir = 'r'};
-    machine.transition_table.transitions[19] = {.currentState = "5", .readSymbol = BLANK, .nextState = "6", .writeSymbol = 'b', .dir = 'l'};
-    
-    machine.transition_table.transitions[20] = {.currentState = "6", .readSymbol = 'a', .nextState = "6", .writeSymbol = 'a', .dir = 'l'};
-    machine.transition_table.transitions[21] = {.currentState = "6", .readSymbol = 'b', .nextState = "6", .writeSymbol = 'b', .dir = 'l'};
-    machine.transition_table.transitions[22] = {.currentState = "6", .readSymbol = '#', .nextState = "7", .writeSymbol = '#', .dir = 'l'};
-    
-    machine.transition_table.transitions[23] = {.currentState = "7", .readSymbol = 'a', .nextState = "7", .writeSymbol = 'a', .dir = 'l'};
-    machine.transition_table.transitions[24] = {.currentState = "7", .readSymbol = 'b', .nextState = "7", .writeSymbol = 'b', .dir = 'l'};
-    machine.transition_table.transitions[25] = {.currentState = "7", .readSymbol = '#', .nextState = "1", .writeSymbol = 'b', .dir = 'l'};
-    
-    machine.transition_table.transitions[26] = {.currentState = "8", .readSymbol = 'a', .nextState = "8", .writeSymbol = BLANK, .dir = 'r'};
-    machine.transition_table.transitions[27] = {.currentState = "8", .readSymbol = 'b', .nextState = "8", .writeSymbol = BLANK, .dir = 'r'};
-    machine.transition_table.transitions[28] = {.currentState = "8", .readSymbol = '#', .nextState = "halt", .writeSymbol = BLANK, .dir = 'r'};
+    for (int k = 0; k < machine.transition_table.size; k++) {
+        Serial.println(machine.transition_table.transitions[k].nextState);
+    }
 }
 
 /**
@@ -176,7 +287,7 @@ void buildMachine() {
 void b_back_pressed() {
     if (millis() - buttonMillis > interval) {
         switch (phase) {
-            case SETUP:
+            case MACHINE_SELECTION:
                 break;
             case INSERT_INPUT:
                 selectSymbolMode = false;
@@ -196,7 +307,10 @@ void b_back_pressed() {
 void b_left_pressed() {
     if (millis() - buttonMillis > interval) {
         switch (phase) {
-        case SETUP:
+        case MACHINE_SELECTION:
+            // Left control to machine selection menu
+            if (selectedMachine == 0) selectedMachine = machines.size-1;
+            else selectedMachine--;
             break;
         case INSERT_INPUT:
             // If tape cell is selected, select the character
@@ -226,7 +340,10 @@ void b_left_pressed() {
 void b_right_pressed() {
     if (millis() - buttonMillis > interval) {
         switch (phase) {
-            case SETUP:
+            case MACHINE_SELECTION:
+                // Right control to machine selection menu
+                if (selectedMachine == machines.size-1) selectedMachine = 0;
+                else selectedMachine++;
                 break;
             case INSERT_INPUT:
                 // If tape cell is selected, select the character
@@ -256,7 +373,11 @@ void b_right_pressed() {
 void b_enter_pressed() {
     if (millis() - buttonMillis > interval) {
         switch (phase) {
-            case SETUP:
+            case MACHINE_SELECTION:
+                phase = INSERT_INPUT;
+                lcd.clear();
+                buildMachine();
+                RGB_color(0, 255, 0);
                 break;
             case INSERT_INPUT:
                 selectSymbolMode = true;
@@ -277,10 +398,15 @@ void b_enter_pressed() {
 void b_next_state_pressed() {
     if (millis() - buttonMillis > interval) {
         switch (phase) {
-            case SETUP:
+            case MACHINE_SELECTION:
+                phase = INSERT_INPUT;
+                lcd.clear();
+                buildMachine();
+                RGB_color(0, 255, 0);
                 break;
             case INSERT_INPUT:
                 phase = RUN;
+                lcd.noBlink();
                 lcd.clear();
                 break;
             case RUN:
@@ -296,10 +422,11 @@ void b_next_state_pressed() {
  * Routine used to compute the input (phase = RUN)
  */
 void run() {
+    RGB_color(255, 165, 0);
     boolean foundNextState = false;
     int i = 0;
     // loop until next state is found or there is no next state
-    while (!foundNextState && i < machine.transition_table.size) {
+    while (!foundNextState && i < machine.transition_table.1size) {
         transition transition = machine.transition_table.transitions[i];
         if (machine.currentState == transition.currentState && transition.readSymbol == machine.tape[machine.headPos]) {
             foundNextState = true;
@@ -316,10 +443,8 @@ void run() {
     
     // if there is no next state, halt
     if (!foundNextState) {
-        //Serial.println("\nState: " + machine.currentState);
-        //Serial.println(String(machine.tape[machine.headPos]));
-        //Serial.println("[*] HALTED [*]");
         phase = HALTED;
+        RGB_color(0, 255, 0);
     }
 }
 
@@ -328,18 +453,23 @@ void run() {
  */
 void draw() {
     switch (phase) {
-        case SETUP:
+        case MACHINE_SELECTION:
+            lcd.setCursor(0,0);
+            lcd.print("Select a machine");
+            lcd.setCursor(0, 1);         
+            lcd.print(machines.names[selectedMachine]);
+            lcd.print("                ");
             break;
         case INSERT_INPUT:
             lcd.setCursor(0, 0);
             
-            // Change draw depending on where is the cursor is
+            // Change the tape display depending on where the cursor is
             if (inputCursor == 0) for (int i = 0; i < 16; i++) lcd.write(machine.tape[machine.headPos + i]);
             else if (inputCursor == 15) for (int i = 0; i < 16; i++) lcd.write(machine.tape[machine.headPos + i - 15]);
             else for (int i = 0; i < 16; i++) lcd.write(machine.tape[machine.headPos + i - inputCursor]);
          
             lcd.setCursor(2, 1);
-            lcd.write("Insert input");
+            lcd.print("Insert input");
             lcd.setCursor(inputCursor, 0);
             break;
         case RUN:
@@ -347,25 +477,36 @@ void draw() {
             for (int i = 0; i < 16; i++) lcd.write(machine.tape[machine.headPos + i - 7]);
             
             lcd.setCursor(0, 1);
-            lcd.write("Processing...");
+            lcd.print("Processing...");
             lcd.setCursor(7, 0);
             break;
          case HALTED:
             lcd.setCursor(0, 1);
-            lcd.write("                ");
+            lcd.print("                ");
             lcd.setCursor(0, 1);
-            lcd.write("Halted");
+            lcd.print("Halted");
             lcd.setCursor(7, 0);
             break;
     }
 }
 
 /**
+ * Changes the rgb led color
+ */
+void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
+{
+    analogWrite(red_light_pin, red_light_value);
+    analogWrite(green_light_pin, green_light_value);
+    analogWrite(blue_light_pin, blue_light_value);
+}
+
+/**
  * Interruption handling and lcd drawing
  */
 void loop() {
+    
     if (millis() - drawMillis > interval) {
-        draw();      
+        draw();
         if (phase == RUN) run();
         drawMillis = millis();
     }
